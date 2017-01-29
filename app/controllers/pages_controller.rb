aa80fbdcdec20c0cefb04841b8429a9aef786242
Andrew Kime <dontaskandy@hotmail.com>
2017-01-29 14:32:43 -0600

    added controllers

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def menu
+  end
+
+  def specials
+  end
+
+  def contact
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
+  get 'pages/menu'
+
+  get 'pages/specials'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
